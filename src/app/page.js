7b9889cf520148e2daa8a342bce6455a3e4ce917
Eric Chen <ericfhchen@gmail.com>
2025-01-30 16:06:42 -0500

    fixed scrolling position

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex items-end justify-center h-[100svh] overflow-hidden">
+    <div className="fixed flex items-end justify-center h-[100svh] overflow-hidden">
       <p className="mb-10 text-xl">LEFT</p>
     </div>
   );
