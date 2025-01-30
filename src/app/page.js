e6de63355d9a314ed6ebf475971b00047a421ab4
Eric Chen <ericfhchen@gmail.com>
2025-01-30 16:59:22 -0500

    attempt to get above IG UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex items-end justify-center h-[100svh] overflow-hidden">
-      <a href="mailto:office@ericlchen.com"><p className="hover:animate-blink font-normal mb-10 text-xl">LEFT</p></a>
+    <div className="flex flex-col items-end justify-center min-h-screen overflow-hidden">
+      <a href="mailto:office@ericlchen.com" className="fixed bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <p className="hover:animate-blink font-normal mb-10 text-xl">LEFT</p>
+      </a>
     </div>
   );
 }
